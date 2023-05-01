ca32dcbfe21d327452f932f79c2645961f907c82
snowkwon420 <snowkwon420@gmail.com>
2023-05-01 16:50:50 +0900

    2023.05.01

---
+++ JavaScript/20230427/vending/js/index.js
@@ -0,0 +1,10 @@
+import ColaGenerator from "./classes/colaGenerator.js";
+import VendingMachineEvents from "./classes/vendingMachineEvents.js";
+
+const colaGenerator = new ColaGenerator();
+const vendingMachineEvents = new VendingMachineEvents();
+
+(async function () {
+  await colaGenerator.setup();
+  vendingMachineEvents.bindEvent();
+});
